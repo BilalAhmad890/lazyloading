a06f57cde69e1e31996f860f8fa988e7355c8982
Bilalahmad02 <bilal.ahma10@gmail.com>
2025-05-19 02:10:14 +0200

    added modifications

--- lazyloadingtesting/shop-baseline/src/main.tsx
+++ lazyloadingtesting/shop-baseline/src/main.tsx
@@ -2,12 +2,14 @@ import * as React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <React.StrictMode>
       <App />
+      <SpeedInsights/>
     </React.StrictMode>,
   );
 } else {
